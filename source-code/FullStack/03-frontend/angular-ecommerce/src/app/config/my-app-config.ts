55bbbd7107620782999677e4c5337297f993c312
Weiqi Zhang <weiqi997998@gmail.com>
2024-04-20 11:02:31 -0400

    NEW

--- source-code/FullStack/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ source-code/FullStack/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oagc9chz6pqWgbIV5d7',
         issuer: 'https://dev-64507843.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
